1cc939ea50c7da4ac0a3dc9f31b65aac1819b05f
Z0KRI <david1166@live.com.mx>
2024-06-16 22:04:37 -0600

    🥅 Error Handler
    Ademas de hacer la configuración de los alias y middlewares en general

--- service/bootstrap/app.php
+++ service/bootstrap/app.php
@@ -1,18 +1,40 @@
 <?php
 
+use Illuminate\Http\Request;
+
+use App\Http\Traits\ErrorHandler;
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->api(prepend: [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        ]);
+
+        $middleware->alias([
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (Throwable $e, Request $request) {
+            $dataValidated = ErrorHandler::validateException($e, $request);
+            $response = [
+                "http" => [
+                    "status" => $dataValidated['code'],
+                    "message" => $dataValidated['message'],
+                    "method" => $request->getMethod(),
+                    "success" => false
+                ],
+                "errors" => $dataValidated['errors'],
+            ];
+            return response()->json($response, $dataValidated['code']);
+        });
     })->create();
