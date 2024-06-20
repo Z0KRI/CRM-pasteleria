482c3a4aad2c76e708f86bc641d4e8f96abb84f2
Z0KRI <david1166@live.com.mx>
2024-06-19 23:43:34 -0600

    ✨ Feat: Ruta de employees

--- service/routes/api.php
+++ service/routes/api.php
@@ -3,8 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\EmployeeController;
 
 Route::prefix('auth')->group(function () {
     Route::post('sign-in', [AuthController::class, 'SignIn']);
     Route::post('sign-out', [AuthController::class, 'SignOut']);
 });
+
+Route::apiResource('employees', EmployeeController::class);
