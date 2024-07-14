f3925c8845d03cd0894c01d1075171bbace655f3
jon013002 <jonmtz3002@gmail.com>
2024-07-14 15:54:16 -0600

    ✨Feat: Ruta de employments (jobs)

--- service/routes/api.php
+++ service/routes/api.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\EmploymentController;
 
 @include('auth.routes.php');
 
 Route::apiResource('employees', EmployeeController::class);
+
+Route::apiResource('employments', EmploymentController::class);
