afde19c51a7b31fb4b91e2bd7ea62be75d9f3bf5
Z0KRI <david1166@live.com.mx>
2024-07-14 16:01:02 -0600

    🦺 ApiTokens

--- service/app/Models/User.php
+++ service/app/Models/User.php
@@ -8,11 +8,12 @@ use Illuminate\Notifications\Notifiable;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable, HasRoles, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
