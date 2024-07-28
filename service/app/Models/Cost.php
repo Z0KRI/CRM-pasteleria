be04227120e3a1bb4b7c96720f1fc1895cdff300
jon013002 <jonmtz3002@gmail.com>
2024-07-27 20:48:12 -0600

    ✨Feat: Costs Model

---
+++ service/app/Models/Cost.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Traits\FilterByQuery;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Cost extends Model
+{
+    use HasFactory, FilterByQuery, HasUuids, SoftDeletes;
+
+    protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'weight',
+        'cost',
+        'price',
+    ];
+}
